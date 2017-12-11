a192e66359d3fef8c35d7ca4d03cff5b465ebc8c
HobbitA <w5meng@edu.uwaterloo.ca>
2017-12-11 04:26:02 -0500

    user-specified website option upgrade

--- js/pblock.js
+++ js/pblock.js
@@ -1,166 +1,117 @@
 $(document).ready(function () {
 
-var blocklist = ["reddit.com", "facebook.com", "tumblr.com", "twitter.com", "youtube.com", "distractify.com", "9gag.com", "4chan.org", "amazon.com", "netflix.com", "imgur.com"];
+var blocklist = ["focusUrlreddit", "focusUrlfacebook", "focusUrltumblr", "focusUrltwitter", "focusUrlyoutube", "focusUrldistractify", "focusUrl9gag", "focusUrl4chan", "focusUrlamazon", "focusUrlnetflix", "focusUrlimgur"];
 var blocksite = ["reddit", "facebook", "tumblr", "twitter", "youtube", "distractify", "9gag", "4chan", "amazon", "netflix", "imgur"];
 
-// bug fix is to store in local storage key url and value array of blocked site domains to be more user-agent friendly...
-function showList () {
-  var i = 0;
-  for (i; i < blocklist.length; i++) {
-      $("#blocklist").append("<div class='togglelink' id='site-"+ i +"'>" + blocklist[i] + "</div>");
+function consoleArr() {
+var i;
+console.log("local storage");
+for (i = 0; i < localStorage.length; i++)   {
+    console.log(localStorage.key(i) + "=[" + localStorage.getItem(localStorage.key(i)) + "]");
   }
 }
 
-// initial view
-showList();
 
+// calc number of blocked url items currently in local storage
+function focusLen () {
+  var arr = [];
 
+  for (var i = 0; i < localStorage.length; i++) {
+    var item = localStorage.key(i);
 
-// option clear all from list and add back functions
-$("#site-0").toggle(
-    function() {
-      localStorage.removeItem("reddit");
-      $("#site-0").css({"text-decoration": "line-through"})
-    },
-
-    function() {
-      localStorage.setItem('reddit', 'reddit.com');
-      $("#site-0").css({"text-decoration": "none"})
-    });
-
-$("#site-1").toggle(
-  function() {
-    localStorage.removeItem("facebook");
-    $("#site-1").css({"text-decoration": "line-through"})
-  },
-
-  function() {
-    localStorage.setItem('facebook', 'facebook.com');
-    $("#site-1").css({"text-decoration": "none"})
-  });
-
-$("#site-2").toggle(
-  function() {
-    localStorage.removeItem("tumblr");
-    $("#site-2").css({"text-decoration": "line-through"})
-  },
-
-  function() {
-    localStorage.setItem('tumblr', 'tumblr.com');
-    $("#site-2").css({"text-decoration": "none"})
-  });
-
-
-$("#site-3").toggle(
-  function() {
-    localStorage.removeItem("twitter");
-    $("#site-3").css({"text-decoration": "line-through"})
-  },
-
-  function() {
-    localStorage.setItem('twitter', 'twitter.com');
-    $("#site-3").css({"text-decoration": "none"})
-  });
-
-$("#site-4").toggle(
-  function() {
-    localStorage.removeItem("youtube");
-    $("#site-4").css({"text-decoration": "line-through"})
-  },
-
-  function() {
-    localStorage.setItem('youtube', 'youtube.com');
-    $("#site-4").css({"text-decoration": "none"})
-  });
-
-$("#site-5").toggle(
-  function() {
-    localStorage.removeItem("distractify");
-    $("#site-5").css({"text-decoration": "line-through"})
-  },
-
-function() {
-  localStorage.setItem('distractify', 'distractify.com');
-  $("#site-5").css({"text-decoration": "none"})
-});
-
-$("#site-6").toggle(
-  function() {
-    localStorage.removeItem("9gag");
-    $("#site-6").css({"text-decoration": "line-through"})
-  },
-
-  function() {
-    localStorage.setItem('9gag', '9gag.com');
-    $("#site-6").css({"text-decoration": "none"})
-  });
-
-$("#site-7").toggle(
-  function() {
-    localStorage.removeItem("4chan");
-    $("#site-7").css({"text-decoration": "line-through"})
-  },
-
-  function() {
-    localStorage.setItem('4chan', '4chan.com');
-    $("#site-7").css({"text-decoration": "none"})
-  });
-
-$("#site-8").toggle(
-  function() {
-    localStorage.removeItem("amazon");
-    $("#site-8").css({"text-decoration": "line-through"})
-  },
-
-function() {
-  localStorage.setItem('amazon', 'amazon.com');
-  $("#site-8").css({"text-decoration": "none"})
-});
-
-$("#site-9").toggle(
-  function() {
-    localStorage.removeItem("netflix");
-    $("#site-9").css({"text-decoration": "line-through"})
-  },
-
-function() {
-  localStorage.setItem('netflix', 'netflix.com');
-  $("#site-9").css({"text-decoration": "none"})
-});
-
-$("#site-10").toggle(
-  function() {
-    localStorage.removeItem("imgur");
-    $("#site-10").css({"text-decoration": "line-through"})
-  },
-
-function() {
-  localStorage.setItem('imgur', 'imgur.com');
-  $("#site-10").css({"text-decoration": "none"})
-});
-
-// Jquery click action to clear all and add back all blocked sites
-
-$("#click").toggle(
-  function() {
-    for (var i = 0; i < blocklist.length; i++)   {
-        localStorage.removeItem(blocksite[i]);
-        $("#site-"+i).css({"display" : "none"});
-        document.getElementById("click").innerHTML = "Undo";
+    if ($.type(item) === "string") {
+      if (item.substr(0,8) === "focusUrl") {
+        arr.push(localStorage.getItem(item));
+      }
     }
-  },
-
-function() {
-  for (var i = 0; i < blocklist.length; i++) {
-
-    localStorage.setItem(blocksite[i], blocklist[i]);
-
-    $("#site-"+i).css({"display" : "block"});
-    document.getElementById("click").innerHTML = "Clear";
   }
+  return arr;
+}
+
+
+
+function showList () {
+  //first need to clear preceding list so that we can update the showList
+  $("#blocklist").children().remove();
+  var urlArray = focusLen();
+
+
+  var i = 0;
+  for (i; i < urlArray.length; i++) {
+      $("#blocklist").append("<div class='togglelink' id='site-"+ i +"'>" + urlArray[i] + "</div>");
+  }
+}
+
+// Initial
+showList();
+//
+
+
+
+function addSite(site) {
+  if((site.substr(site.length - 4) === ".com")||
+  (site.substr(site.length - 4) === ".org")||
+  (site.substr(site.length - 4) === ".net")||
+  (site.substr(site.length - 4) === ".int")||
+  (site.substr(site.length - 4) === ".edu")||
+  (site.substr(site.length - 4) === ".gov")||
+  (site.substr(site.length - 4) === ".mil") && (site != "chrome://extensions/")) {
+
+
+    localStorage.setItem("focusUrl" + site, site);
+    showList();
+
+  }
+}
+
+
+// store flagged items in an array, then at the end loop through and remove all flagged, that way not removing as indexing
+function removeAll () {
+  var localLen = localStorage.length;
+  var del = [];
+  var i = 0;
+  while (i < localLen) {
+    var item = localStorage.key(i);
+
+    if ($.type(item) === "string") { // just because .key is user agent dependent so need to be careful
+      if (item.substr(0,8) === "focusUrl") {
+        del.push(item);
+      }
+    }
+    i++;
+  }
+  for (var  i = 0; i < del.length; i++) {
+    localStorage.removeItem(del[i]);
+  }
+}
+// jQuery to detect if user clicked Clear, don't want to delete the url item, just clear the contents on the array value
+$("#click").click(function() {
+  removeAll();
+  showList();
 });
 
 
+// jQueryto detect if user adds another site to be blocked
+$("#blockbutton").click(function() {
+  var add = $("#blacklist").val();
+  addSite(add);
+  showList();
+});
+
+
+//add back original defualt blocked sites to localStorage when user clicks
+$("#clickDefault").click(function() {
+  removeAll();
+
+  for (var i = 0; i < blocklist.length; i++) {
+    if (blocklist[i].substr(8) === "4chan") {
+      localStorage.setItem(blocklist[i], blocklist[i].substr(8) + ".org");
+      continue;
+    }
+    localStorage.setItem(blocklist[i], blocklist[i].substr(8) + ".com");
+  }
+  showList();
+});
 
 
 
--- js/pblock2.js
+++ js/pblock2.js
@@ -1,14 +1,17 @@
-localStorage.setItem('reddit', 'reddit.com');
-localStorage.setItem('facebook', 'facebook.com');
-localStorage.setItem('tumblr', 'tumblr.com');
-localStorage.setItem('twitter', 'twitter.com');
-localStorage.setItem('youtube', 'youtube.com');
-localStorage.setItem('distractify', 'distractify.com');
-localStorage.setItem('9gag', '9gag.com');
-localStorage.setItem('4chan', '4chan.com');
-localStorage.setItem('amazon', 'amazon.com');
-localStorage.setItem('netflix', 'netflix.com');
-localStorage.setItem('imgur', 'imgur.com');
+
+var blocklist = ["reddit.com", "facebook.com", "tumblr.com", "twitter.com", "youtube.com", "distractify.com", "9gag.com", "4chan.org", "amazon.com", "netflix.com", "imgur.com"];
+
+localStorage.setItem('focusUrlreddit', 'reddit.com');
+localStorage.setItem('focusUrlfacebook', 'facebook.com');
+localStorage.setItem('focusUrltumblr', 'tumblr.com');
+localStorage.setItem('focusUrltwitter', 'twitter.com');
+localStorage.setItem('focusUrlyoutube', 'youtube.com');
+localStorage.setItem('focusUrldistractify', 'distractify.com');
+localStorage.setItem('focusUrl9gag', '9gag.com');
+localStorage.setItem('focusUrl4chan', '4chan.org');
+localStorage.setItem('focusUrlamazon', 'amazon.com');
+localStorage.setItem('focusUrlnetflix', 'netflix.com');
+localStorage.setItem('focusUrlimgur', 'imgur.com');
 
 
 
@@ -20,22 +23,37 @@ for (i = 0; i < localStorage.length; i++)   {
 }
 
 
-chrome.tabs.onUpdated.addListener(function(tabId, changedInfo, tab) {
 
-  $.each(localStorage, function(key, value){
-    var url = localStorage.getItem(localStorage.key(key));
-    if (tab.url.match(url)) {
-      chrome.tabs.update(tabId, {"url" : chrome.extension.getURL("block.html")}, function () {});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changedInfo, tab) {
+  for (var i = 0; i < localStorage.length; i++) {
+    var item = localStorage.key(i);
+    // start at 8 because every url key for the focus chrome app will have an obligatory focusUrl identifier string attatched to it for organizational purposes
+    if ($.type(item) === "string") {
+      if (item.substr(0,8) === "focusUrl") {
+        if (tab.url.match(item.substr(8))) {
+          chrome.tabs.update(tabId, {"url" : chrome.extension.getURL("block.html")}, function () {});
+          break;
+        }
+      }
     }
-  });
+  }
+
 });
 
 
 chrome.tabs.onCreated.addListener(function(tab) {
-  $.each(localStorage, function(key, value){
-      var url = localStorage.getItem(localStorage.key(key));
-      if (tab.url.match(url)) {
-        chrome.tabs.update(tabId, {"url" : chrome.extension.getURL("block.html")}, function () {});
+  for (var i = 0; i < localStorage.length; i++) {
+    var item = localStorage.key(i);
+    // start at 8 because every url key for the focus chrome app will have an obligatory focusUrl identifier string attatched to it for organizational purposes
+    if ($.type(item) === "string") {
+      if (item.substr(0,8) === "focusUrl") {
+        if (tab.url.match(item.substr(8))) {
+          chrome.tabs.update(tabId, {"url" : chrome.extension.getURL("block.html")}, function () {});
+          break;
+        }
       }
-    });
-    });
+    }
+  }
+
+});
